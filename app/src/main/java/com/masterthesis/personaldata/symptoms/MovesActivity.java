79d512d2dd2026538957afbe197e49c1b43e2459
Konstantinos Michail <koumou276@gmail.com>
2016-02-14 00:35:00 +0100

    refactored

--- app/src/main/java/com/masterthesis/personaldata/symptoms/MovesActivity.java
+++ app/src/main/java/com/masterthesis/personaldata/symptoms/MovesActivity.java
@@ -5,177 +5,22 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.Spinner;
 import android.widget.TextView;
 
-import com.midhunarmid.movesapi.MovesAPI;
-import com.midhunarmid.movesapi.MovesHandler;
-import com.midhunarmid.movesapi.activity.ActivityData;
-import com.midhunarmid.movesapi.activity.TrackPointsData;
+import com.masterthesis.personaldata.symptoms.managers.MovesManager;
 import com.midhunarmid.movesapi.auth.AuthData;
-import com.midhunarmid.movesapi.profile.ProfileData;
-import com.midhunarmid.movesapi.segment.SegmentData;
-import com.midhunarmid.movesapi.storyline.StorylineData;
-import com.midhunarmid.movesapi.summary.SummaryData;
-import com.midhunarmid.movesapi.summary.SummaryListData;
-import com.midhunarmid.movesapi.util.MovesStatus;
-
-import java.util.ArrayList;
 
 public class MovesActivity extends AppCompatActivity implements View.OnClickListener {
     private static final String TAG = "Moves";
-
-    private static final String CLIENT_ID = "U2hOIga3gar6ftAXiHzJ0e1YWAi0tNUF";
-    private static final String CLIENT_SECRET = "JPYX6Xf57bG6j4y1JyfDdp25jQpDDo8Ds01Oh2oX0xDA0LkS86a4ul6BdPf21b8P";
-    private static final String CLIENT_REDIRECTURL = "http://dfuinspector.com/";
-    private static final String CLIENT_SCOPES = "activity location";
-    private static final int REQUEST_AUTHORIZE = 1;
-
+    private MovesManager movesManager = MovesManager.getInstance();
     private Spinner mSpinnerAPI;
     private Button mButtonSubmit;
     private ProgressBar mProgressRequest;
     private TextView mTvResponse;
-    private MovesHandler<AuthData> authDialogHandler = new MovesHandler<AuthData>() {
-        @Override
-        public void onSuccess(AuthData arg0) {
-            toggleProgress(false);
-            updateResponse("Access Token : " + arg0.getAccessToken() + "\n"
-                    + "Expires In : " + arg0.getExpiresIn() + "\n"
-                    + "User ID : " + arg0.getUserID());
-        }
-
-        @Override
-        public void onFailure(MovesStatus status, String message) {
-            toggleProgress(false);
-            updateResponse("Request Failed! \n"
-                    + "Status Code : " + status + "\n"
-                    + "Status Message : " + message + "\n\n"
-                    + "Specific Message : " + status.getStatusMessage());
-        }
-    };
-    private MovesHandler<ProfileData> profileHandler = new MovesHandler<ProfileData>() {
-
-        @Override
-        public void onSuccess(ProfileData arg0) {
-            toggleProgress(false);
-            updateResponse("User ID : " + arg0.getUserID()
-                    + "\nUser Platform : " + arg0.getPlatform());
-        }
-
-        @Override
-        public void onFailure(MovesStatus status, String message) {
-            toggleProgress(false);
-            updateResponse("Request Failed! \n"
-                    + "Status Code : " + status + "\n"
-                    + "Status Message : " + message + "\n\n"
-                    + "Specific Message : " + status.getStatusMessage());
-        }
-    };
-    private MovesHandler<ArrayList<SummaryListData>> summaryHandler = new MovesHandler<ArrayList<SummaryListData>>() {
-        @Override
-        public void onSuccess(ArrayList<SummaryListData> result) {
-            toggleProgress(false);
-            for (SummaryListData sld : result) {
-                Log.i(TAG, "Calories Idle : " + sld.getCaloriesIdle());
-                Log.i(TAG, "Last update : " + sld.getLastUpdate());
-                Log.i(TAG, "Date : " + sld.getDate());
-
-                ArrayList<SummaryData> summaries = sld.getSummaries();
-                for (SummaryData sd : summaries) {
-                    Log.i(TAG, "Activity : " + sd.getActivity());
-                    Log.i(TAG, "Calories : " + sd.getCalories());
-                    Log.i(TAG, "Distance : " + sd.getDistance());
-                    Log.i(TAG, "Duration : " + sd.getDuration());
-                    Log.i(TAG, "Group : " + sd.getGroup());
-                    Log.i(TAG, "Steps : " + sd.getSteps());
-                }
-
-            }
-            updateResponse("Summary Items : " + result.size());
-        }
-
-        @Override
-        public void onFailure(MovesStatus status, String message) {
-            toggleProgress(false);
-            updateResponse("Request Failed! \n"
-                    + "Status Code : " + status + "\n"
-                    + "Status Message : " + message + "\n\n"
-                    + "Specific Message : " + status.getStatusMessage());
-        }
-    };
-    private MovesHandler<ArrayList<StorylineData>> storylineHandler = new MovesHandler<ArrayList<StorylineData>>() {
-        @Override
-        public void onSuccess(ArrayList<StorylineData> result) {
-            toggleProgress(false);
-            for (StorylineData sld : result) {
-                Log.i(TAG, "Calories Idle : " + sld.getCaloriesIdle());
-                Log.i(TAG, "Last update : " + sld.getLastUpdate());
-                Log.i(TAG, "Date : " + sld.getDate());
-
-                ArrayList<SegmentData> segments = sld.getSegments();
-                for (SegmentData sd : segments) {
-                    Log.i(TAG, "Segments start time : " + sd.getStartTime());
-                    Log.i(TAG, "Segments end time : " + sd.getEndTime());
-                    Log.i(TAG, "Segments type : " + sd.getType());
-                    if (sd.getPlace() != null) {
-                        Log.i(TAG, "Segments place foursquare: " + sd.getPlace().getFoursquareId());
-                        Log.i(TAG, "Segments place type : " + sd.getPlace().getType());
-                        Log.i(TAG, "Segments place name: " + sd.getPlace().getName());
-                        Log.i(TAG, "Segments place foursquare id : " + sd.getPlace().getFoursquareCategoryIds());
-                        Log.i(TAG, "Segments place location : " + sd.getPlace().getLocation());
-                    }
-                    ArrayList<ActivityData> activities = sd.getActivities();
-                    for (ActivityData ad : activities) {
-                        Log.i(TAG, "Activities: name : " + ad.getActivity());
-                        Log.i(TAG, "Activities: start time : " + ad.getStartTime());
-                        Log.i(TAG, "Activities: end time : " + ad.getEndTime());
-                        Log.i(TAG, "Activities: calories : " + ad.getCalories());
-                        Log.i(TAG, "Activities: manual : " + ad.getManual());
-                        Log.i(TAG, "Activities: distance : " + ad.getDistance());
-                        Log.i(TAG, "Activities: duration : " + ad.getDuration());
-                        Log.i(TAG, "Activities: group : " + ad.getGroup());
-                        Log.i(TAG, "Activities: steps : " + ad.getSteps());
-                        ArrayList<TrackPointsData> trackPoints = ad.getTrackPoints();
-                        for (TrackPointsData tpd : trackPoints) {
-                            Log.i(TAG, "Track points" + tpd.getLat());
-                            Log.i(TAG, "Track points" + tpd.getLat());
-                            Log.i(TAG, "Track points" + tpd.getLat());
-                        }
-                        Log.i(TAG, "Activities: start time : " + ad.getTrackPoints());
-
-
-                    }
-
-                }
-
-                ArrayList<SummaryData> summaries = sld.getSummary();
-                for (SummaryData sd : summaries) {
-                    Log.i(TAG, "Activity : " + sd.getActivity());
-                    Log.i(TAG, "Calories : " + sd.getCalories());
-                    Log.i(TAG, "Distance : " + sd.getDistance());
-                    Log.i(TAG, "Duration : " + sd.getDuration());
-                    Log.i(TAG, "Group : " + sd.getGroup());
-                    Log.i(TAG, "Steps : " + sd.getSteps());
-                }
-
-            }
-
-            updateResponse("Summary Items : " + result.size());
-        }
-
-        @Override
-        public void onFailure(MovesStatus status, String message) {
-            toggleProgress(false);
-            updateResponse("Request Failed! \n"
-                    + "Status Code : " + status + "\n"
-                    + "Status Message : " + message + "\n\n"
-                    + "Specific Message : " + status.getStatusMessage());
-        }
-    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -200,22 +45,20 @@ public class MovesActivity extends AppCompatActivity implements View.OnClickList
 
         mButtonSubmit.setOnClickListener(this);
 
-        try {
-            MovesAPI.init(this, CLIENT_ID, CLIENT_SECRET, CLIENT_SCOPES, CLIENT_REDIRECTURL);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        movesManager.init(this);
+
     }
 
     @Override
     public void onClick(View v) {
-        toggleProgress(true);
+//        toggleProgress(true);
         switch (mSpinnerAPI.getSelectedItemPosition()) {
             case 0: // Authenticate
-                MovesAPI.authenticate(authDialogHandler, MovesActivity.this);
+//                MovesAPI.authenticate(movesManager.getAuthDialogHandler(), MovesActivity.this);
+                movesManager.authenticate(MovesActivity.this);
                 break;
             case 1: // Get Auth Data
-                AuthData auth = MovesAPI.getAuthData();
+                AuthData auth = movesManager.getAuthData();
                 if (auth != null) {
                     updateResponse("Access Token : " + auth.getAccessToken() + "\n"
                             + "Expires In : " + auth.getExpiresIn() + "\n"
@@ -223,71 +66,70 @@ public class MovesActivity extends AppCompatActivity implements View.OnClickList
                 } else {
                     updateResponse("Not Authenticated Yet!");
                 }
-                toggleProgress(false);
+//                toggleProgress(false);
                 break;
             case 2: // Get Profile
-                MovesAPI.getProfile(profileHandler);
+                movesManager.profile();
                 break;
             case 4: // Get Summary Day
-                MovesAPI.getSummary_SingleDay(summaryHandler, "20160212", null);
+                movesManager.summarySingleDay("20160212");
                 break;
             case 5: // Get Summary Week
-                MovesAPI.getSummary_SpecificWeek(summaryHandler, "2016-W06", null);
+                movesManager.summaryWeek("2016-W06");
                 break;
             case 6: // Get Summary Month
-                MovesAPI.getSummary_SpecificMonth(summaryHandler, "201602", null);
+                movesManager.summaryMonth("201602");
                 break;
             case 7: // Get Summary Range
-                MovesAPI.getSummary_WithinRange(summaryHandler, "20160211", "20160212", null);
+                movesManager.summaryRange("20160211", "20160212");
                 break;
             case 8: // Get Summary PastDays
-                MovesAPI.getSummary_PastDays(summaryHandler, "2", null);
+                movesManager.summaryPastDays("31");
                 break;
             case 10: // Get Storyline Day
-                MovesAPI.getStoryline_SingleDay(storylineHandler, "20160212", null, false);
+                movesManager.storylineDay("20160212",true);
                 break;
             case 11: // Get Storyline Week
-                MovesAPI.getStoryline_SpecificWeek(storylineHandler, "2016-W06", null, false);
+                movesManager.storylineWeek("2016-W06",true);
                 break;
             case 12: // Get Storyline Month
-                MovesAPI.getStoryline_SpecificMonth(storylineHandler, "201601", null, false);
+                movesManager.storylineMonth("201602");
                 break;
             case 13: // Get Storyline Range
-                MovesAPI.getStoryline_WithinRange(storylineHandler, "20160210", "20160212", null, true);
+                movesManager.storylineRange("20160211", "20160212",true);
                 break;
             case 14: // Get Storyline PastDays
-                MovesAPI.getStoryline_PastDays(storylineHandler, "2", null, true);
+                movesManager.storylinePastDays("7",true);
                 break;
             case 16: // Get Activities Day
-                MovesAPI.getActivities_SingleDay(storylineHandler, "20160212", null);
+                movesManager.activitiesDay("20160212");
                 break;
             case 17: // Get Activities Week
-                MovesAPI.getActivities_SpecificWeek(storylineHandler, "2016-W06", null);
+                movesManager.activitiesWeek("2016-W06");
                 break;
             case 18: // Get Activities Month
-                MovesAPI.getActivities_SpecificMonth(storylineHandler, "201602", "20140314T073812Z");
+                movesManager.activitiesMonth("201602");
                 break;
             case 19: // Get Activities Range
-                MovesAPI.getActivities_WithinRange(storylineHandler, "20160211", "20160212", null);
+                movesManager.activitiesRange("20160211", "20160212");
                 break;
             case 20: // Get Activities PastDays
-                MovesAPI.getActivities_PastDays(storylineHandler, "31", null);
+                movesManager.activitiesPastDays("31");
                 break;
             default:
-                toggleProgress(false);
                 break;
         }
     }
 
-    public void toggleProgress(final boolean isProgrressing) {
-        runOnUiThread(new Runnable() {
-            @Override
-            public void run() {
-                mProgressRequest.setVisibility(isProgrressing ? View.VISIBLE : View.GONE);
-                mButtonSubmit.setVisibility(isProgrressing ? View.GONE : View.VISIBLE);
-            }
-        });
-    }
+//    public void toggleProgress(final boolean isProgressing) {
+//        runOnUiThread(new Runnable() {
+//            @Override
+//            public void run() {
+//                mProgressRequest.setVisibility(isProgressing ? View.VISIBLE : View.GONE);
+//                mButtonSubmit.setVisibility(isProgressing ? View.GONE : View.VISIBLE);
+//            }
+//        });
+//    }
 
     public void updateResponse(final String message) {
         runOnUiThread(new Runnable() {
--- app/src/main/java/com/masterthesis/personaldata/symptoms/managers/MovesManager.java
+++ app/src/main/java/com/masterthesis/personaldata/symptoms/managers/MovesManager.java
@@ -1,9 +1,304 @@
 package com.masterthesis.personaldata.symptoms.managers;
 
-/**
- * Created by Konstantinos Michail on 2/13/2016.
- */
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import com.midhunarmid.movesapi.MovesAPI;
+import com.midhunarmid.movesapi.MovesHandler;
+import com.midhunarmid.movesapi.activity.ActivityData;
+import com.midhunarmid.movesapi.activity.TrackPointsData;
+import com.midhunarmid.movesapi.auth.AuthData;
+import com.midhunarmid.movesapi.profile.ProfileData;
+import com.midhunarmid.movesapi.segment.SegmentData;
+import com.midhunarmid.movesapi.storyline.StorylineData;
+import com.midhunarmid.movesapi.summary.SummaryData;
+import com.midhunarmid.movesapi.summary.SummaryListData;
+import com.midhunarmid.movesapi.util.MovesStatus;
+
+import java.util.ArrayList;
+
 public class MovesManager {
+
+    private static final String TAG = "Moves";
+
+    private static final String CLIENT_ID = "U2hOIga3gar6ftAXiHzJ0e1YWAi0tNUF";
+    private static final String CLIENT_SECRET = "JPYX6Xf57bG6j4y1JyfDdp25jQpDDo8Ds01Oh2oX0xDA0LkS86a4ul6BdPf21b8P";
+    private static final String CLIENT_REDIRECTURL = "http://dfuinspector.com/";
+    private static final String CLIENT_SCOPES = "activity location";
+
+    private static MovesManager movesManager = new MovesManager();
+    private MovesHandler<AuthData> authDialogHandler = new MovesHandler<AuthData>() {
+        @Override
+        public void onSuccess(AuthData arg0) {
+            Log.i(TAG, "Access Token : " + arg0.getAccessToken() + "\n"
+                    + "Expires In : " + arg0.getExpiresIn() + "\n"
+                    + "User ID : " + arg0.getUserID());
+        }
+
+        @Override
+        public void onFailure(MovesStatus status, String message) {
+            Log.i(TAG, "Request Failed! \n"
+                    + "Status Code : " + status + "\n"
+                    + "Status Message : " + message + "\n\n"
+                    + "Specific Message : " + status.getStatusMessage());
+        }
+    };
+    private MovesHandler<ProfileData> profileHandler = new MovesHandler<ProfileData>() {
+
+        @Override
+        public void onSuccess(ProfileData arg0) {
+            Log.i(TAG, "User ID : " + arg0.getUserID()
+                    + "\nUser Platform : " + arg0.getPlatform());
+        }
+
+        @Override
+        public void onFailure(MovesStatus status, String message) {
+            Log.i(TAG, "Request Failed! \n"
+                    + "Status Code : " + status + "\n"
+                    + "Status Message : " + message + "\n\n"
+                    + "Specific Message : " + status.getStatusMessage());
+        }
+    };
+    private MovesHandler<ArrayList<SummaryListData>> summaryHandler = new MovesHandler<ArrayList<SummaryListData>>() {
+        @Override
+        public void onSuccess(ArrayList<SummaryListData> result) {
+            for (SummaryListData sld : result) {
+                Log.i(TAG, "Calories Idle : " + sld.getCaloriesIdle());
+                Log.i(TAG, "Last update : " + sld.getLastUpdate());
+                Log.i(TAG, "Date : " + sld.getDate());
+
+                ArrayList<SummaryData> summaries = sld.getSummaries();
+                for (SummaryData sd : summaries) {
+                    Log.i(TAG, "Activity : " + sd.getActivity());
+                    Log.i(TAG, "Calories : " + sd.getCalories());
+                    Log.i(TAG, "Distance : " + sd.getDistance());
+                    Log.i(TAG, "Duration : " + sd.getDuration());
+                    Log.i(TAG, "Group : " + sd.getGroup());
+                    Log.i(TAG, "Steps : " + sd.getSteps());
+                }
+
+            }
+        }
+
+        @Override
+        public void onFailure(MovesStatus status, String message) {
+            Log.i(TAG, "Request Failed! \n"
+                    + "Status Code : " + status + "\n"
+                    + "Status Message : " + message + "\n\n"
+                    + "Specific Message : " + status.getStatusMessage());
+        }
+    };
+    private MovesHandler<ArrayList<StorylineData>> storylineHandler = new MovesHandler<ArrayList<StorylineData>>() {
+        @Override
+        public void onSuccess(ArrayList<StorylineData> result) {
+            for (StorylineData sld : result) {
+                Log.i(TAG, "-Calories Idle : " + sld.getCaloriesIdle());
+                Log.i(TAG, "-Last update : " + sld.getLastUpdate());
+                Log.i(TAG, "-Date : " + sld.getDate());
+
+                ArrayList<SegmentData> segments = sld.getSegments();
+                for (SegmentData sd : segments) {
+                    Log.i(TAG, "-Segments start time : " + sd.getStartTime());
+                    Log.i(TAG, "-Segments end time : " + sd.getEndTime());
+                    Log.i(TAG, "-Segments type : " + sd.getType());
+                    if (sd.getPlace() != null) {
+                        Log.i(TAG, "-Segments place foursquare: " + sd.getPlace().getFoursquareId());
+                        Log.i(TAG, "-Segments place type : " + sd.getPlace().getType());
+                        Log.i(TAG, "-Segments place name: " + sd.getPlace().getName());
+                        Log.i(TAG, "-Segments place foursquare id : " + sd.getPlace().getFoursquareCategoryIds());
+                        Log.i(TAG, "-Segments place location lat : " + sd.getPlace().getLocation().getLat());
+                        Log.i(TAG, "-Segments place location lon : " + sd.getPlace().getLocation().getLon());
+                    }
+                    ArrayList<ActivityData> activities = sd.getActivities();
+                    for (ActivityData ad : activities) {
+                        Log.i(TAG, "--Activities: name : " + ad.getActivity());
+                        Log.i(TAG, "--Activities: start time : " + ad.getStartTime());
+                        Log.i(TAG, "--Activities: end time : " + ad.getEndTime());
+                        Log.i(TAG, "--Activities: calories : " + ad.getCalories());
+                        Log.i(TAG, "--Activities: manual : " + ad.getManual());
+                        Log.i(TAG, "--Activities: distance : " + ad.getDistance());
+                        Log.i(TAG, "--Activities: duration : " + ad.getDuration());
+                        Log.i(TAG, "--Activities: group : " + ad.getGroup());
+                        Log.i(TAG, "--Activities: steps : " + ad.getSteps());
+                        ArrayList<TrackPointsData> trackPoints = ad.getTrackPoints();
+                        for (TrackPointsData tpd : trackPoints) {
+                            Log.i(TAG, "---Track points" + tpd.getLat() + " ; " + tpd.getLon());
+                            Log.i(TAG, "---Track points" + tpd.getTime());
+                        }
+
+
+                    }
+
+                }
+
+                ArrayList<SummaryData> summaries = sld.getSummary();
+                for (SummaryData sd : summaries) {
+                    Log.i(TAG, "-Summary Activity : " + sd.getActivity());
+                    Log.i(TAG, "-Summary Calories : " + sd.getCalories());
+                    Log.i(TAG, "-Summary Distance : " + sd.getDistance());
+                    Log.i(TAG, "-Summary Duration : " + sd.getDuration());
+                    Log.i(TAG, "-Summary Group : " + sd.getGroup());
+                    Log.i(TAG, "-Summary Steps : " + sd.getSteps());
+                }
+
+            }
+
+        }
+
+        @Override
+        public void onFailure(MovesStatus status, String message) {
+            Log.i(TAG, "Request Failed! \n"
+                    + "Status Code : " + status + "\n"
+                    + "Status Message : " + message + "\n\n"
+                    + "Specific Message : " + status.getStatusMessage());
+        }
+    };
+
+    private MovesManager() {
+    }
+
+    public static MovesManager getInstance() {
+        return movesManager;
+    }
+
+
+    public void init(Context _context) {
+        try {
+            MovesAPI.init(_context, CLIENT_ID, CLIENT_SECRET, CLIENT_SCOPES, CLIENT_REDIRECTURL);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void authenticate(AppCompatActivity activity) {
+        MovesAPI.authenticate(movesManager.authDialogHandler, activity);
+    }
+
+    public AuthData getAuthData() {
+
+        return MovesAPI.getAuthData();
+    }
+
+    public void profile() {
+        MovesAPI.getProfile(movesManager.profileHandler);
+    }
+
+    /**
+     * @param day Format example "20160212"
+     */
+    public void summarySingleDay(String day) {
+        MovesAPI.getSummary_SingleDay(movesManager.summaryHandler, day, null);
+    }
+
+    /**
+     * @param week Format example "2016-W06"
+     */
+    public void summaryWeek(String week) {
+        MovesAPI.getSummary_SpecificWeek(movesManager.summaryHandler, week, null);
+    }
+
+    /**
+     * @param month Format example "201602"
+     */
+    public void summaryMonth(String month) {
+        MovesAPI.getSummary_SpecificMonth(movesManager.summaryHandler, month, null);
+    }
+
+    /**
+     * @param start Format example "20160211"
+     * @param end   Format example "20160212"
+     */
+    public void summaryRange(String start, String end) {
+        MovesAPI.getSummary_WithinRange(movesManager.summaryHandler, start, end, null);
+    }
+
+    /**
+     * @param pastDays Format example "2"
+     */
+    public void summaryPastDays(String pastDays) {
+        MovesAPI.getSummary_PastDays(movesManager.summaryHandler, pastDays, null);
+
+    }
+
+    /**
+     * @param day Format example "20160212"
+     * @param trp To return track points or not
+     */
+    public void storylineDay(String day, boolean trp) {
+        MovesAPI.getStoryline_SingleDay(movesManager.storylineHandler, day, null, trp);
+    }
+
+    /**
+     * @param week Format example "2016-W06"
+     * @param trp  To return track points or not
+     */
+    public void storylineWeek(String week, boolean trp) {
+        MovesAPI.getStoryline_SpecificWeek(movesManager.storylineHandler, week, null, trp);
+    }
+
+    /**
+     * @param month Format example "201602"
+     */
+    public void storylineMonth(String month) {
+        MovesAPI.getStoryline_SpecificMonth(movesManager.storylineHandler, month, null, false);
+    }
+
+    /**
+     * @param start Format example "20160211"
+     * @param end   Format example "20160212"
+     * @param trp   To return track points or not
+     */
+    public void storylineRange(String start, String end, boolean trp) {
+        MovesAPI.getStoryline_WithinRange(movesManager.storylineHandler, start, end, null, trp);
+    }
+
+    /**
+     * @param pastDays Format example "2"
+     * @param trp      To return track points or not
+     */
+    public void storylinePastDays(String pastDays, boolean trp) {
+        MovesAPI.getStoryline_PastDays(movesManager.storylineHandler, pastDays, null, trp);
+    }
+
+    /**
+     * @param day Format example "20160212"
+     */
+    public void activitiesDay(String day) {
+        MovesAPI.getActivities_SingleDay(movesManager.storylineHandler, day, null);
+    }
+
+    /**
+     * @param week Format example "2016-W06"
+     */
+    public void activitiesWeek(String week) {
+        MovesAPI.getActivities_SpecificWeek(movesManager.storylineHandler, week, null);
+    }
+
+    /**
+     * @param month Format example "201602"
+     */
+    public void activitiesMonth(String month) {
+        MovesAPI.getActivities_SpecificMonth(movesManager.storylineHandler, "201602", null);
+    }
+
+    /**
+     * @param start Format example "20160211"
+     * @param end   Format example "20160212"
+     */
+    public void activitiesRange(String start, String end) {
+        MovesAPI.getActivities_WithinRange(movesManager.storylineHandler, "20160211", "20160212", null);
+    }
+
+    /**
+     * @param pastDays Format example "2"
+     */
+    public void activitiesPastDays(String pastDays) {
+        MovesAPI.getActivities_PastDays(movesManager.storylineHandler, "31", null);
+    }
 
 
 }
+
+
+
